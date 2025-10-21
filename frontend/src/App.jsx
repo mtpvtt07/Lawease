c1fb8e8220c9f63434864e72a2a83b4cee204205
kaivalya <kaivalya775@gmail.com>
2025-10-21 13:09:21 +0530

    feat: integrate Legal Chatbot using Google GenAI for legal assistance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import MainPage from './pages/MainPage';
+import OtpPage from './pages/OtpPage';
+import LegalChatBot from './pages/LegalChatBot';
 import Footer from './components/footer';
 import { Routes, Route } from 'react-router-dom';
 
@@ -11,7 +14,10 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/otp" element={<OtpPage />} />
       </Routes>
+      <LegalChatBot />
       <Footer />
     </>
   );
